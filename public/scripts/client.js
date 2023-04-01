8f9ea921d2a2e3ea3f9bb703027fc1c72aa4ce34
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-04-01 20:31:17 +0000

    Modified styles and script files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,101 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+// Fake data taken from initial-tweets.json
+
+$(document).ready(function() {
+//hide error element tag 
+$('.error').hide()
+
+
+const escapeText = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}; 
+
+
+function createTweetElement(object) {
+  let $tweet = $('<article/>').addClass("tweet");
+  const html = `
+  <header class="header-tweet">
+  <div class="_spaceTab">
+  <div class="left-margin">
+  <img class="avatar" src="${object.user.avatars}" alt="${object.user.name}">
+  <span class="name">${object.user.name}</span>
+  </div>
+  <span class="handle"> ${object.user.handle}</span>
+  </div>
+  </header>
+  <section class="tweets">${escapeText(object.content.text)}</section>
+  <footer class="_tweet">
+  <span class="time"> ${timeago.format(object.created_at)}</span>
+  <span class="icons">
+  <span class="flag">
+  <i class="fa-solid fa-flag" ></i>
+  </span>
+  <span class="retweet">
+  <i class="fa-sharp fa-solid fa-retweet"></i>
+  </span>
+  <span class="heart">
+  
+  <i class="fa-solid fa-heart"></i>
+  </span>
+  </span>
+  </footer>
+  `
+
+  $tweet.html(html);
+  return $tweet;
+}
+
+const renderTweets = function(tweets) {
+  const container = $('#tweets-container')
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    container.append($tweet);
+  }
+}
+
+const loadTweets = function() {
+  $.ajax("/tweets", { method: 'GET' })
+    .then((response) => {
+      console.log('RESPONSE', response)
+      renderTweets(response);
+    })
+}
+
+loadTweets();
+
+$('#submit-form').submit(function(event) {
+  event.preventDefault();
+  const data = $('#tweet-text').serialize();
+  console.log('DATA--->', data)
+  if (data.length === 0) {
+    alert('tweet not present')
+    return false
+  } if (data.length > 140) {
+   $('.error').slideDown();
+  } else {
+    $('.error').hide()
+    $.ajax({
+      type: 'POST',
+      url: '/tweets',
+      data: data,
+      success: function() {
+        loadTweets();
+        $('#tweet-text').val('')
+      },
+      error: function(xhr, status, error) {
+        console.error(error);
+      }
+    });
+    console.log('data', data);
+  }
+})
+
+
+
+})
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,13 @@ $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     const tweet = $(this).val();
     const $counter = $(this).parent().find('.counter');
-    // console.log('LENGTH', tweet.length)
-    //console.log('TEST', $counter.val(140 - tweet.length))
     $counter.val(140 - tweet.length)
-    console.log('COUNTER', $counter.val())
     if ( $counter.val() < 0) {
       $('.counter').css('color', 'red')
+     
     } else {
       $('.counter').css('color','#545149')
     }
 
-    //console.log($(this).parent().find('.counter'))
   })
 })
